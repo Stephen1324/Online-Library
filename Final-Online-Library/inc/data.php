85b4646d62cf2572c4001ea643b71aa2462e0340
Stephen1324 <53320116+Stephen1324@users.noreply.github.com>
2019-12-11 18:29:25 -0500

    Add files via upload

---
+++ Final-Online-Library/inc/data.php
@@ -0,0 +1,165 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "The Hunger Game",
+	"img" => "img/media/thehungergames.jpg",
+    "genre" => "Epic Poetry",
+    "format" => "Hardcover",
+    "year" => 2000,
+    "category" => "Books",
+    "authors" => [
+        "Seamus Heaney"
+    ],
+    "publisher" => "Farrar, Straus and Giroux",
+    "isbn" => '9780374111199'
+];
+$catalog[102] = [
+    "title" => "The Outsiders",
+    "img" => "img/media/theoutsiders.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 2004,
+    "category" => "Books",
+    "authors" => [
+        "F. Scott Fitzgerald"
+    ],
+    "publisher" => "Scribner",
+    "isbn" => '9780743273565'
+];
+$catalog[103] = [
+    "title" => "The Maze Runner",
+    "img" => "img/media/mazerunner.jpg",
+    "genre" => "Gothic Fiction",
+    "format" => "Paperback",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "Gaston Leroux"
+    ],
+    "publisher" => "Barnes & Noble",
+    "isbn" => '9781593082499'
+];
+$catalog[104] = [
+    "title" => "Scorch Trials",
+    "img" => "img/media/scorchtrials.jpg",
+    "genre" => "Horror",
+    "format" => "Paperback",
+    "year" => 2005,
+    "category" => "Books",
+    "authors" => [
+        "Mary Shelley"
+    ],
+    "publisher" => "Barnes & Noble",
+    "isbn" => '9781593081157'
+];
+//Movies
+$catalog[201] = [
+    "title" => "John Wick 3",
+    "img" => "img/media/johnwick3.jpg",
+    "genre" => "Action",
+    "format" => "DVD/Streaming",
+    "year" => 2019,
+    "category" => "Movies",
+    "director" => "Chad Stahelski",
+    "writers" => [
+        "Derek Kolstad",
+    ],
+    "stars" => [
+        "Keanu Reeves",
+        "Halle Barry",
+        "Laurence Fishburne",
+    ]
+];
+$catalog[202] = [
+    "title" => "How To Train A dragon Hidden World",
+    "img" => "img/media/hiddenworld.jpg",
+    "genre" => "Fantasy",
+    "format" => "DVD/Streaming",
+    "year" => 2019,
+    "category" => "Movies",
+    "director" => "Dean DeBlois",
+    "writers" => [
+        "Dean Deblois",
+    ],
+    "stars" => [
+			"Jay Baruchel",
+			"America Ferrera",
+			"Cate Blanchett"
+    ]
+];
+$catalog[203] = [
+    "title" => "The Avengers Endgames",
+    "img" => "img/media/avengersendgame.jpg",
+    "genre" => "Action",
+    "format" => "DVD",
+    "year" => 2019,
+    "category" => "Movies",
+    "director" => "Anthony Russo",
+    "writers" => [
+        "Stan Lee",
+        "Jack Kirby"
+    ],
+    "stars" => [
+			"Robert Downey Jr.",
+			"Chris Evan",
+			"Chris Hemsworth"
+    ]
+];
+$catalog[204] = [
+    "title" => "The house with a Clock in it Wall",
+		"img" => "img/media/thehouswithaclock.jpg",
+	  "genre" => "Drama, Fantasy, Adventure",
+	  "format" => "DVD/Streaming",
+	  "year" => 2019,
+	  "category" => "Movies",
+	  "director" => "Eli Roth",
+	  "writers" => [
+	      "John Bellars"
+
+	  ],
+	  "stars" => [
+	      "Jack Black ",
+	      "Cate Blanchett",
+	      "Owen Vaccaro"
+	  ]
+	];
+//Music
+$catalog[301] = [
+	"title" => "Eminem",
+	"img" => "img/media/Eminem.jpg",
+	"genre" => "Hip-Hop, Rap",
+	"format" => "CD",
+	"year" => 2008,
+	"category" => "Music",
+	"artist" => "Eminem"
+];
+
+$catalog[302] = [
+	"title" => "The Carter IV",
+	"img" => "img/media/LilWayne.jpg",
+	"genre" => " Hip-Hop, Rap",
+	"format" => "Streaming Services",
+	"year" => 2019,
+	"category" => "Music",
+	"artist" => "Lil Wayne"
+];
+$catalog[303] = [
+	"title" => "N.W.A",
+	"img" => "img/media/NWA.jpg",
+	"genre" => " Hip-Hop, Rap",
+	"format" => "CD, Streaming Services",
+	"year" => 1996,
+	"category" => "Music",
+	"artist" => "N.W.A"
+];
+$catalog[304] = [
+	"title" => "50 Cent: Get Rich or Die Tryin ",
+	"img" => "img/media/50Cent.jpeg",
+	"genre" => "Hip-Hop, Rap",
+	"format" => "CD, Streaming Services",
+	"year" => 2005,
+	"category" => "Music",
+	"artist" => "50 Cent"
+];
+?>
